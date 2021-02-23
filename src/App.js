786e024c6ab02dcd679fde0ecdd6bb894ad4d826
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-02-23 10:17:52 +0100

    finish tutorial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,66 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+  const APP_ID = "6a590d97";
+  const APP_KEX = "d9c91774f1dc3786b87403f6ea4113ae";
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
+
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      'https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}'
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(setSearch);
+    setSearch('');
+  }
+
+  return(
     <div className="App">
+      <h1>Hello React</h1>
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="recipes">
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
